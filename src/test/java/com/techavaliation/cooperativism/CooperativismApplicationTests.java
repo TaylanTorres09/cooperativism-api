a1be141f544617f3ba6a4e3ec4bcd1362a2461ad
TaylanTorres09 <taylantorres@gmail.com>
2023-02-18 20:22:18 -0300

    initiate project

---
+++ src/test/java/com/techavaliation/cooperativism/CooperativismApplicationTests.java
@@ -0,0 +1,13 @@
+package com.techavaliation.cooperativism;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CooperativismApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
