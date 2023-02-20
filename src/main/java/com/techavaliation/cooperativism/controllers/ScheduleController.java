eb28d95835a31727c4d1e44221e59db780e5ebb3
TaylanTorres09 <taylantorres@gmail.com>
2023-02-20 14:42:52 -0300

    Add endpoint to create

--- src/main/java/com/techavaliation/cooperativism/controllers/ScheduleController.java
+++ src/main/java/com/techavaliation/cooperativism/controllers/ScheduleController.java
@@ -1,13 +1,20 @@
 package com.techavaliation.cooperativism.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.techavaliation.cooperativism.dtos.ScheduleDTO;
 import com.techavaliation.cooperativism.models.ScheduleModel;
 import com.techavaliation.cooperativism.services.ScheduleService;
+
+import jakarta.validation.Valid;
 
 @RestController
 @RequestMapping("/schedule")
@@ -19,6 +26,12 @@ public class ScheduleController {
     @GetMapping("/{id}")
     public ScheduleModel findByIdSchedule(@PathVariable Long id) {
         return this.scheduleService.findByIdSchedule(id);
+    }
+
+    @PostMapping("/create")
+    public ResponseEntity<ScheduleModel> createSchedule(@Valid @RequestBody ScheduleDTO scheduleDTO) {
+        ScheduleModel schedule = this.scheduleService.createSchedule(scheduleDTO);
+        return new ResponseEntity<>(schedule, HttpStatus.CREATED);
     }
 
 }
