3a0300913c7b90f0fc648bbbe5feaddefa0865f1
TaylanTorres09 <taylantorres@gmail.com>
2023-02-20 14:42:38 -0300

    Treatment MethodArgumentNotValidException Error

--- src/main/java/com/techavaliation/cooperativism/controllers/exceptions/ControllerExceptionHandler.java
+++ src/main/java/com/techavaliation/cooperativism/controllers/exceptions/ControllerExceptionHandler.java
@@ -1,7 +1,11 @@
 package com.techavaliation.cooperativism.controllers.exceptions;
 
+import java.util.List;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
@@ -17,4 +21,15 @@ public class ControllerExceptionHandler {
         StandardError standardError = new StandardError(System.currentTimeMillis(), HttpStatus.NOT_FOUND.value(), e.getMessage());
         return new ResponseEntity<StandardError>(standardError, HttpStatus.NOT_FOUND);
     }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<StandardError> validationError(MethodArgumentNotValidException e, ServletRequest request) {
+        List<FieldError> listFieldError = e.getBindingResult().getFieldErrors();
+        String message = listFieldError.stream().map(er -> er.getDefaultMessage()).toList().get(0);
+
+        StandardError error = new StandardError(System.currentTimeMillis(), HttpStatus.BAD_REQUEST.value(), message);
+
+        return new ResponseEntity<StandardError>(error, HttpStatus.BAD_REQUEST);
+    }
+
 }
