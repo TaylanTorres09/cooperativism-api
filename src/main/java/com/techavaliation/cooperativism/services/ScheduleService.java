3fdd15858af76ce2a63ccc316ec2f57a8e878475
TaylanTorres09 <taylantorres@gmail.com>
2023-02-20 14:42:05 -0300

    Create method to create Schedule

--- src/main/java/com/techavaliation/cooperativism/services/ScheduleService.java
+++ src/main/java/com/techavaliation/cooperativism/services/ScheduleService.java
@@ -1,8 +1,10 @@
 package com.techavaliation.cooperativism.services;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.techavaliation.cooperativism.dtos.ScheduleDTO;
 import com.techavaliation.cooperativism.models.ScheduleModel;
 import com.techavaliation.cooperativism.repositories.ScheduleRepository;
 import com.techavaliation.cooperativism.services.exceptions.ObjectNotFound;
@@ -13,9 +15,16 @@ public class ScheduleService {
     @Autowired
     private ScheduleRepository scheduleRepository;
 
+    @Autowired
+    private ModelMapper mapper;
+
     public ScheduleModel findByIdSchedule(Long id) {
         return this.scheduleRepository.findById(id)
                 .orElseThrow(() -> new ObjectNotFound("Pauta: " + id + ", não encontrada."));
+    }
+
+    public ScheduleModel createSchedule(ScheduleDTO scheduleDTO) {
+        return this.scheduleRepository.save(mapper.map(scheduleDTO, ScheduleModel.class));
     }
 
 }
