75e413268c5eb311df94da0d1e3c9c8b12828720
TaylanTorres09 <taylantorres@gmail.com>
2023-02-21 18:46:52 -0300

    return uri in end-point

--- src/main/java/com/techavaliation/cooperativism/controllers/ScheduleController.java
+++ src/main/java/com/techavaliation/cooperativism/controllers/ScheduleController.java
@@ -1,7 +1,8 @@
 package com.techavaliation.cooperativism.controllers;
 
+import java.net.URI;
+
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -9,6 +10,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.techavaliation.cooperativism.dtos.ScheduleDTO;
 import com.techavaliation.cooperativism.models.ScheduleModel;
@@ -31,7 +33,11 @@ public class ScheduleController {
     @PostMapping("/create")
     public ResponseEntity<ScheduleModel> createSchedule(@Valid @RequestBody ScheduleDTO scheduleDTO) {
         ScheduleModel schedule = this.scheduleService.createSchedule(scheduleDTO);
-        return new ResponseEntity<>(schedule, HttpStatus.CREATED);
+
+        URI uri = ServletUriComponentsBuilder.fromCurrentContextPath().path(String.format("/schedule/%d", schedule.getId())).buildAndExpand(schedule.getId()).toUri();
+        
+        //return uri in headers
+        return ResponseEntity.created(uri).build();
     }
 
 }
