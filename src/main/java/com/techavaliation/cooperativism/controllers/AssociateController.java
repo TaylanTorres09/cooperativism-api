18df4bc78440faf5c907684f77c73f01b5e98608
TaylanTorres09 <taylantorres@gmail.com>
2023-02-21 18:41:18 -0300

    Create method and end-point POST

--- src/main/java/com/techavaliation/cooperativism/controllers/AssociateController.java
+++ src/main/java/com/techavaliation/cooperativism/controllers/AssociateController.java
@@ -1,13 +1,22 @@
 package com.techavaliation.cooperativism.controllers;
 
+import java.net.URI;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import com.techavaliation.cooperativism.dtos.AssociateDTO;
 import com.techavaliation.cooperativism.models.AssociateModel;
 import com.techavaliation.cooperativism.services.AssociateService;
+
+import jakarta.validation.Valid;
 
 @RestController
 @RequestMapping("/associate")
@@ -19,6 +28,16 @@ public class AssociateController {
     @GetMapping("/{id}")
     public AssociateModel findByIdAssociate(@PathVariable Long id) {
         return this.associateService.findByIdAssociate(id);
+    }
+
+    @PostMapping("/create")
+    public ResponseEntity<AssociateModel> create(@Valid @RequestBody AssociateDTO associateDTO) {
+        AssociateModel associate = this.associateService.create(associateDTO);
+
+        URI uri = ServletUriComponentsBuilder.fromCurrentContextPath().path(String.format("/associate/%d", associate.getId())).buildAndExpand(associate.getId()).toUri();
+        
+        //return uri in headers
+        return ResponseEntity.created(uri).build();
     }
 
 }
--- src/main/java/com/techavaliation/cooperativism/services/AssociateService.java
+++ src/main/java/com/techavaliation/cooperativism/services/AssociateService.java
@@ -4,6 +4,7 @@ import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.techavaliation.cooperativism.dtos.AssociateDTO;
 import com.techavaliation.cooperativism.models.AssociateModel;
 import com.techavaliation.cooperativism.repositories.AssociateRepository;
 import com.techavaliation.cooperativism.services.exceptions.ObjectNotFound;
@@ -14,9 +15,16 @@ public class AssociateService {
     @Autowired
     private AssociateRepository associateRepository;
 
+    @Autowired
+    private ModelMapper mapper;
+
     public AssociateModel findByIdAssociate(Long id) {
         return this.associateRepository.findById(id)
                 .orElseThrow(() -> new ObjectNotFound("Associado id: " + id + " não existe"));
+    }
+
+    public AssociateModel create(AssociateDTO associateDTO) {
+        return this.associateRepository.save(mapper.map(associateDTO, AssociateModel.class));
     }
 
 }
