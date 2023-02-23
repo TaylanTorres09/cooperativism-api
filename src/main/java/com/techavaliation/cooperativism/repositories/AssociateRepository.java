87a97a45310552d0883766710ccb98ad17001740
TaylanTorres09 <taylantorres@gmail.com>
2023-02-23 10:27:57 -0300

    Query to find Associate by CPF

--- src/main/java/com/techavaliation/cooperativism/repositories/AssociateRepository.java
+++ src/main/java/com/techavaliation/cooperativism/repositories/AssociateRepository.java
@@ -1,11 +1,16 @@
 package com.techavaliation.cooperativism.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import com.techavaliation.cooperativism.models.AssociateModel;
 
 @Repository
 public interface AssociateRepository extends JpaRepository<AssociateModel, Long>{
+
+    @Query("SELECT associate FROM AssociateModel associate WHERE associate.cpf = :cpf")
+    AssociateModel findByCPF(@Param(value = "cpf") String cpf);
     
 }
